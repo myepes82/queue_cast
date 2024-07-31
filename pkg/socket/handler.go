8082c67c6b1c771a7ff6e5e033ff75c61517f373
Marlon Yepes <marlondevjs@gmail.com>
2024-07-31 15:41:22 -0500

    Temporal commit - Socket structure variatioons"

--- pkg/socket/handler.go
+++ pkg/socket/handler.go
@@ -2,16 +2,25 @@ package socket
 
 import (
 	"fmt"
-	"log"
+	"net/http"
 	"queuecast/pkg/core"
 	"queuecast/pkg/interfaces"
 
-	"golang.org/x/net/websocket"
+	"github.com/gorilla/websocket"
 )
 
 type SocketHandler struct {
 	logger     *core.Logger
 	compressor interfaces.Compressor
+}
+
+var upgrader = websocket.Upgrader{
+	HandshakeTimeout:  1000,
+	ReadBufferSize:    1024,
+	WriteBufferSize:   1024,
+	Error:             nil,
+	CheckOrigin:       func(r *http.Request) bool { return true },
+	EnableCompression: true,
 }
 
 func NewSocketHandler(logger *core.Logger, compressor interfaces.Compressor) *SocketHandler {
@@ -21,41 +30,42 @@ func NewSocketHandler(logger *core.Logger, compressor interfaces.Compressor) *So
 	}
 }
 
-func (s *SocketHandler) HandleSocketConnections(ws *websocket.Conn) {
-	s.logger.Info("New WebSocket connection established")
+func (s *SocketHandler) writeMessage(conn *websocket.Conn, message string) error {
+	compressedMessage, err := s.compressor.Compress([]byte(message))
 
-	defer func() {
-		s.logger.Info("WebSocket connection closed")
-		err := ws.Close()
+	if err != nil {
+		s.logger.Error(fmt.Sprintf("Error decompressing message: %v", err))
+		return err
+	}
+
+	if err := websocket.Message.Send(ws, compressedMessage); err != nil {
+		s.logger.Error(fmt.Sprintf("Error sending message: %v", err))
+		break
+	}
+}
+
+func (s *SocketHandler) readMessage(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
 		if err != nil {
+			s.logger.Error(fmt.Sprintf("Error receiving message: %v", err))
 			return
 		}
-	}()
 
-	for {
-		var compressedMessage []byte
-		if err := websocket.Message.Receive(ws, &compressedMessage); err != nil {
-			s.logger.Error(fmt.Sprintf("Error receiving message: %v", err))
-			break
-		}
-
-		message, err := s.compressor.Compress(compressedMessage)
-		if err != nil {
-			s.logger.Error(fmt.Sprintf("Error decompressing message: %v", err))
-			break
-		}
-		s.logger.Info(fmt.Sprintf("Received message: %v", string(message)))
-		log.Println("Received message:", string(message))
-
-		compressedMessage, err = s.compressor.Compress(message)
-		if err != nil {
-			s.logger.Error(fmt.Sprintf("Error compressing message: %v", err))
-			break
-		}
-
-		if err := websocket.Message.Send(ws, compressedMessage); err != nil {
-			s.logger.Error(fmt.Sprintf("Error sending message: %v", err))
-			break
-		}
+		s.logger.Info(fmt.Sprintf("message received: %v", messageType))
+		s.logger.Info(fmt.Sprintf("message received: %v", string(p)))
 	}
+}
+
+func (s *SocketHandler) HandleSocketConnections(w http.ResponseWriter, r *http.Request) {
+
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		s.logger.Error(fmt.Sprintf("error upgrading websocket: %v", err))
+		return
+	}
+
+	s.logger.Info("New WebSocket connection established")
+
+	s.readMessage(ws)
 }
